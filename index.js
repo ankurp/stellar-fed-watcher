662ab4d1ad5f4ce0fa16d798d8a98cfa0b92bd50
Ankur Patel <ankur.patel@ymail.com>
2018-06-29 16:08:00 -0400

    Indenting

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const client = require('./cache');
 
 const server = new StellarSdk.Server('https://horizon.stellar.org');
 const closeStream = server.payments()
-      .cursor('now')
-      .stream({
-        onmessage: (msg) => {
-          if (msg.type !== 'payment') return;
+  .cursor('now')
+  .stream({
+    onmessage: (msg) => {
+      if (msg.type !== 'payment') return;
 
-          const { to } = msg;
-          client.get(to, function(err, email) {
-            if (err) {
-              console.error(`Error with get request for ${to}`);
-              return;
-            }
-            if (!email) return;
-
-            mailer.send({
-              to: email,
-              text: `You received ${msg.amount} Stellar lumens from ${msg.from}
-              \nFor more details view here: https://horizon.stellar.org/transactions/${msg.transaction_hash}`
-            });
-          });
+      const { to } = msg;
+      client.get(to, function(err, email) {
+        if (err) {
+          console.error(`Error with get request for ${to}`);
+          return;
         }
+        if (!email) return;
+
+        mailer.send({
+          to: email,
+          text: `You received ${msg.amount} Stellar lumens from ${msg.from}
+          \nFor more details view here: https://horizon.stellar.org/transactions/${msg.transaction_hash}`
+        });
       });
+    }
+  });
 
 process.on('SIGINT', () => {
   closeStream();
