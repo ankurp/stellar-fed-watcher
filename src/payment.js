f59e9a3c1dbe325cf20b6f836cf4485f842e2268
Ankur Patel <apatel@shutterstock.com>
2018-09-14 19:29:57 -0400

    Adding memo information when available

--- src/payment.js
+++ src/payment.js
@@ -1,14 +1,18 @@
 const mailer = require('./mailer');
 const client = require('./cache');
-const StellarDomain = require('./domain');
+const fetch = require('./fetch');
 
 const paymentHandler = (msg) => {
   const {
+    _links: {
+      transaction: {
+        href: transactionUrl
+      }
+    },
     to: account,
     amount,
     from,
-    asset_code: assetCode = 'XLM',
-    transaction_hash: transactionHash
+    asset_code: assetCode = 'XLM'
   } = msg;
   console.log(`PAYMENTS: ${account} received payment of ${amount} ${assetCode}`);
   client.smembers(account, (err, emails) => {
@@ -16,14 +20,23 @@ const paymentHandler = (msg) => {
       console.error('Error fetching from cache');
       return;
     }
-    emails.forEach((email) => {
-      console.log(`Sending payment notification email to ${email} for account ${account}`);
-      mailer.send({
-        to: email,
-        subject: 'You received payment via Stellar',
-        text: `You just received ${amount} ${assetCode} from ${from}
-        \nFor more details view here: ${StellarDomain}/transactions/${transactionHash}
-        \nTo stop receiving these email please update the settings in your account on stellarfed.org`
+    fetch(transactionUrl).then(({ memo, memo_type: memoType }) => {
+      const senderInfo = [from];
+      if (memoType === 'text' && memo) {
+        senderInfo.push(`with memo "${memo}"`);
+      }
+
+      emails.forEach((email) => {
+        console.log(`Sending payment notification email to ${email} for account ${account}`);
+        mailer.send({
+          to: email,
+          subject: 'You received payment via Stellar',
+          text: `You just received ${amount} ${assetCode} from ${senderInfo.join(' ')}
+
+          For more details view here: ${transactionUrl}
+
+          To stop receiving these email please update the settings in your account on stellarfed.org`
+        });
       });
     });
   });
