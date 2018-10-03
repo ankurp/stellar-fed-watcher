cfe9b252a5961330582e3195afc28dd8fc10e4ad
Ankur Patel <apatel@shutterstock.com>
2018-10-03 13:11:03 -0400

    Updating logic

--- src/stellar-stream.js
+++ src/stellar-stream.js
@@ -3,18 +3,27 @@ const client = require('./cache');
 const StellarDomain = require('./domain');
 const server = new StellarSdk.Server(StellarDomain);
 
-const stellarStream = (onmessage) => {
-  client.get('CURSOR', (err, cursor) => {
+const stellarStream = (onMessage) => {
+  client.get('CURSOR', (err, savedCursor = 'now') => {
+    let cursor = savedCursor;
     if (err) {
       console.error('Error fetching last processed cursor', err);
       return;
     }
 
     const closeStream = server.payments()
-      .cursor(cursor || 'now')
-      .stream({ onmessage });
+      .cursor(cursor)
+      .stream({
+        onmessage: (msg) => {
+          onMessage(msg);
+          cursor = msg.id;
+        }
+      });
 
     process.on('SIGINT', () => {
+      if (cursor) {
+        client.set('CURSOR', cursor);
+      }
       closeStream();
       client.quit();
     });
--- src/watcher.js
+++ src/watcher.js
@@ -1,7 +1,6 @@
 const accountHandler = require('./account');
 const paymentHandler = require('./payment');
 const stellarStream = require('./stellar-stream');
-const client = require('./cache');
 
 stellarStream((msg) => {
   switch (msg.type) {
@@ -9,7 +8,6 @@ stellarStream((msg) => {
     case 'create_account':  accountHandler(msg); break;
     default:
   }
-  client.set('CURSOR', msg.id);
 });
 
 module.exports = () => 'Stellar Watcher Up and Watching';
